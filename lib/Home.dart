a28d29da4c3ef32d072a3644dedc5f10e6e6d545
Eduard <edu-ard@hotmail.es>
2024-04-25 21:41:18 +0200

    Errores a la hora de inferir

---
+++ lib/Home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class Homescreen extends StatefullWidget{
+    const Homescreen ({super.key});
+
+  State<Homescreen> createstate(){
+        return _Homescreenstate();
+  }
+
+
+}
+
+class  _Homescreenstate() extends State<Homescreen>{
+
+
+
+}
+
+
